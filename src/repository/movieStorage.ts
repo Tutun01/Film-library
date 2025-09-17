14df4ef479dc2e380c9c3f1045a8d3ecc6b6ca73
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-09-17 12:45:41 +0200

    * Create movieStorage

---
+++ src/repository/movieStorage.ts
@@ -0,0 +1,33 @@
+import {SingleMovieSearch} from "../interfaces/movieStorage/singleMovieSearch";
+
+export function rememberMovieSearch(movie: SingleMovieSearch):void {
+
+
+
+    const existingMovies = getAllMovieSearches();
+    const alredyExists:boolean = existingMovies.some((m:SingleMovieSearch) =>
+        m.name.toLowerCase() === movie.name.toLowerCase()
+        && m.year === movie.year
+    );
+
+    if (alredyExists) {
+
+        return;
+    }
+
+    existingMovies.push(movie);
+    localStorage.setItem("rememberedMovies", JSON.stringify(existingMovies));
+
+
+}
+
+export function getAllMovieSearches(): SingleMovieSearch [] {
+
+    const data = localStorage.getItem("rememberedMovies");
+
+    return data ? JSON.parse(data) : [];
+}
+
+export function clearMovieStorage():void {
+    localStorage.removeItem("rememberedMovies");
+}
