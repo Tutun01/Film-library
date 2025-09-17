a9e06951a1e0e99e9e75dfc3eaa2310d4ddbc40d
Stefan Tutunovic <stefan.tutunovic11@gmail.com>
2025-09-17 12:45:55 +0200

    *

--- dist/services/omdbApiService.d.ts
+++ dist/services/omdbApiService.d.ts
@@ -1,6 +1,4 @@
 import type { ApiParamsInterface } from "../interfaces/ApiParamsInterface";
 import type { ApiResponseInterface } from "../interfaces/ApiResponseInterface";
-import type { omdbMoviesInterface } from "../interfaces/omdbMoviesInterface";
 export declare function callOMDBApi(params: ApiParamsInterface[]): Promise<ApiResponseInterface>;
-export declare function getMovieElement(movieElements: omdbMoviesInterface[]): HTMLDivElement;
 //# sourceMappingURL=omdbApiService.d.ts.map
